cbda4b0f4d4561b25e98439023544b164a93db93
Klaas Jan Wierenga <k.j.wierenga@gmail.com>
2017-12-20 10:11:09 +0100

    refactor to class and add reconnect with exponential backoff

--- lib/spotify/client.rb
+++ lib/spotify/client.rb
@@ -2,31 +2,83 @@ require 'dotenv/load'
 require 'faye/websocket'
 require 'eventmachine'
 
-def applescript(script)
-  system 'osascript', *script.split(/\n/).map { |line| ['-e', line] }.flatten
-end
+class Spotify::Client
 
-EM.run {
-  ws = Faye::WebSocket::Client.new(ENV.fetch('WEBSOCKET'))
+  PING_INTERVAL = 1.freeze
+  MAX_RETRIES   = 7.freeze
 
-  ws.on :open do |event|
-    p [:open]
-    # ws.send('play')
-    # ws.send('pause')
+  def initialize
+    @ws = nil
+    @retries = 0
+    em_run
   end
 
-  ws.on :message do |event|
-    p [:message, event.data]
+  def applescript(script)
+    system 'osascript', *script.split(/\n/).map { |line| ['-e', line] }.flatten
+  end
 
+  def connect
+    p [:connect]
+    @ws = Faye::WebSocket::Client.new(ENV.fetch('WEBSOCKET'), nil, ping: PING_INTERVAL)
+
+    @ws.on :any do |event|
+      p event
+    end
+
+    @ws.on :open do |event|
+      p [:open]
+      @retries = 0
+    end
+
+    # EM.add_periodic_timer(PING_INTERVAL) do
+    #   @ws.ping do
+    #     p [:pong]
+    #   end
+    # end
+
+    @ws.on :message do |event|
+      p [:message, event.data]
+      if 'close' == event.data
+        @ws.close
+      else
+        run_applescript(event.data)
+      end
+    end
+
+    @ws.on :error do |event|
+      p [:error, event.message]
+    end
+
+    @ws.on :close do |event|
+      p [:close, event.code, event.reason]
+      @ws = nil
+      reconnect
+    end
+  end
+
+  def reconnect
+    if @retries < MAX_RETRIES
+      @retries += 1
+      p [:reconnect, @retries]
+      EM.add_timer(2 ** @retries) do
+        connect
+      end
+    else
+      raise "Timeout after #{@retries} retries."
+    end
+  end
+
+  def run_applescript(command)
     applescript <<-END
       tell application "Spotify"
-        #{event.data}
+        #{command}
       end tell
     END
   end
 
-  ws.on :close do |event|
-    p [:close, event.code, event.reason]
-    ws = nil
+  def em_run
+    EM.run { connect }
   end
-}
+end
+
+Spotify::Client.new
